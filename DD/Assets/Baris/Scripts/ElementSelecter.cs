784f18a098fb837678c8f674840ad1b125aefe42
Barış Boz <wbb7179623@gmail.com>
2024-02-25 02:32:28 +0300

    DragonDeneme

---
+++ DD/Assets/Baris/Scripts/ElementSelecter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ElementSelecter : MonoBehaviour
+{
+    [SerializeField] Button[] buttons;
+    private void Update() {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            
+        }
+    }
+
+}
