1b29b8c7741c5f9bf2be0f7153092d2c84241c01
Barış Boz <wbb7179623@gmail.com>
2024-02-24 06:52:21 +0300

    CharacterContoller
    Update0.1

---
+++ DD/Assets/Baris/Scripts/PlayerController.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D),typeof(Collider2D))]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Veribles")]
+    [SerializeField] float speed;   
+    [SerializeField] float jumpForce;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private Transform groundCheck;
+    bool isWallSliding;
+    float wallSlidingSpeed=2;
+
+    bool isWallJumping;
+    float wallJumpingDirection;
+    float wallJumpingTime=0.2f;
+    float wallJumpingCounter;
+    float wallJumpingDuration=0.4f;
+    [SerializeField] Vector2 wallJumpingPower=new Vector2(8,16);
+    private bool isFacingRight = true;
+
+    Vector2 Dir;
+
+    [Header("Layers")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] LayerMask wallLayer; 
+
+    Collider2D _collider2D;
+    Rigidbody2D _rigidbody2D;
+    private void Start() {
+        _collider2D=GetComponent<Collider2D>();
+        _rigidbody2D=GetComponent<Rigidbody2D>();
+    }
+    private void Update() {
+        
+        Dir.x=Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)&&IsGrounded())
+        {
+            Jump();
+        }
+        WallSlide();      
+        WallJump();
+        if (!isWallJumping)
+        {
+            Flip();
+        }
+    }
+    private void FixedUpdate() {
+        
+        if (!isWallJumping)
+        {
+            _rigidbody2D.velocity=new Vector2(Dir.x*speed,_rigidbody2D.velocity.y);
+        }
+                   
+    }
+
+
+    
+    void Jump(){
+        _rigidbody2D.velocity=Vector2.up*jumpForce;
+    }
+    void WallSlide(){
+        if (!IsGrounded()&&IsWalled()&&Dir.x!=0)
+        {
+            isWallSliding=true;
+            _rigidbody2D.velocity=new Vector2(_rigidbody2D.velocity.x,Mathf.Clamp(_rigidbody2D.velocity.y,-wallSlidingSpeed,float.MaxValue));
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    private void WallJump()
+    {
+        if (isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            
+            isWallJumping = true;
+            _rigidbody2D.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if (transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+
+    bool IsGrounded(){
+        return Physics2D.OverlapCircle(groundCheck.position,0.2f,groundLayer);
+    }
+    bool IsWalled(){
+        return Physics2D.OverlapCircle(wallCheck.position,0.2f, wallLayer);;         
+    }
+     private void Flip()
+    {
+        if (isFacingRight && Dir.x < 0f || !isFacingRight && Dir.x > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+    private void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(new Vector2(transform.position.x+.5f,transform.position.y),0.2f);
+        Gizmos.DrawWireCube(new Vector2(transform.position.x+0.5f,transform.position.y),new Vector2(0.2f,0.2f));
+    }
+}
