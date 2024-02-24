fed6e6c1e46a8c38e3e7893edcf9ea51d2284caa
Barış Boz <wbb7179623@gmail.com>
2024-02-24 12:15:53 +0300

    CharacterControllerV4

--- DD/Assets/Baris/Scripts/Health.cs
+++ DD/Assets/Baris/Scripts/Health.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public float MaxHealt,CurrentHealt;
+    [SerializeField] GameObject bloodParticle;
+    [SerializeField] float MaxHealt,CurrentHealt;
     private void Start() {
         CurrentHealt=MaxHealt;
     }
     public void TakeDamage(float damage ){
-        CurrentHealt=-damage;
+        CurrentHealt-=damage;
+        Instantiate(bloodParticle,new Vector3(transform.position.x,transform.position.y+1,transform.position.z),Quaternion.identity);
         if (CurrentHealt<=0)
         {
             Destroy(this.gameObject,3);        
--- DD/Assets/Baris/Scripts/PlayerAttack.cs
+++ DD/Assets/Baris/Scripts/PlayerAttack.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Power")]
+    [SerializeField] float attackCD;
+    [SerializeField] float damage;
+    [SerializeField] float attackDuration;
+    [SerializeField] float attackRange;
+    [SerializeField] Transform attackPosition;
+    [SerializeField] LayerMask whatIsEnemy;
+    private void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Attack());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    IEnumerator Attack(){
+        Collider2D[] enemysToDamage= Physics2D.OverlapCircleAll(attackPosition.position,attackRange,whatIsEnemy);
         
+        yield return new WaitForSeconds(attackDuration); 
+        for (int i = 0; i < enemysToDamage.Length; i++)
+        {
+            enemysToDamage[i].GetComponent<Health>().TakeDamage(damage);
+
+        }
+        yield return new WaitForSeconds(attackCD);
+    }
+    private void OnDrawGizmosSelected() {
+        if (attackPosition!=null)
+        {
+            Gizmos.DrawWireSphere(attackPosition.position,attackRange);            
+        }
+
     }
 }
